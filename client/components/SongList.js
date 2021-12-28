d334e97de12e502c2661ac56193864bdec09b0d7
kawgh1 <krseven@gmail.com>
2021-12-27 21:42:45 -0600

    added graphql query

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,7 +1,22 @@
 import React from "react";
+// graphql
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
-function SongList() {
+function SongList(props) {
+    console.log(props);
     return <div>SongList</div>;
 }
 
-export default SongList;
+const query = gql`
+    {
+        songs {
+            title
+        }
+    }
+`;
+
+// This code calls the query function which immediately returns a function and calls it on Songlist
+// This bonds our GraphQL Query to our Component just before rendering it to the screen
+// the data returned from the GraphQL query is placed inside the Component's props
+export default graphql(query)(SongList);
