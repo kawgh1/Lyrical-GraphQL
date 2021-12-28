c215caf62489acfa3f4b7967036591b8e50bb04c
kawgh1 <krseven@gmail.com>
2021-12-27 19:59:50 -0600

    setting up MongoDB Cloud server cluster, connecting application, hiding DB access variable with dotenv

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ Starter project from a GraphQL course by Stephen Grider on Udemy.com
 
 -   Webpack server is responsible for taking all the client side React code and shipping it down to the browser
     ![lyrical-graphql-architecture](https://raw.githubusercontent.com/kawgh1/Lyrical-GraphQL/main/diagrams/lyrical-graphql-architecture.png)
+
+# Tools
+
+-   Dotenv for hiding environment (MongoDB user access) variables
+    **npm install dotenv**
+-   https://appdividend.com/2019/06/06/what-is-process-env-in-node-js-environment-variables-in-node-js/
--- server/server.js
+++ server/server.js
@@ -1,33 +1,54 @@
-const express = require('express');
-const models = require('./models');
-const expressGraphQL = require('express-graphql');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const schema = require('./schema/schema');
+const express = require("express");
+const models = require("./models");
+const expressGraphQL = require("express-graphql");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const schema = require("./schema/schema");
 
 const app = express();
 
-// Replace with your mongoLab URI
-const MONGO_URI = '';
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+const DBuser = process.env.MONGO_USER;
+const DBpassword = process.env.MONGO_PASSWORD;
+
+console.log("DBuser = " + DBuser);
+
+// Replace with your Atlas MongoDB Cloud URI
+const MONGO_URI =
+    "mongodb+srv://" +
+    DBuser +
+    ":" +
+    DBpassword +
+    "@cluster0.llhyi.mongodb.net/Lyrical-GraphQL?retryWrites=true&w=majority";
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+    throw new Error("You must provide a Atlas MongoDB Cloud URI");
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+    .once("open", () =>
+        console.log("Connected to Atlas MongoDB Cloud instance.")
+    )
+    .on("error", (error) =>
+        console.log("Error connecting to Atlas MongoDB Cloud:", error)
+    );
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(
+    "/graphql",
+    expressGraphQL({
+        schema,
+        graphiql: true,
+    })
+);
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpack = require("webpack");
+const webpackConfig = require("../webpack.config.js");
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
